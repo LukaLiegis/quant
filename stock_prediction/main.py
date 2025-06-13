b2770e5a0bfb80e10d8b1cf32db2c1487a3c01c8
lukaliegis <luka.liegis@gmail.com>
2025-06-13 16:26:19 +0300

    data preparation

--- stock_prediction/main.py
+++ stock_prediction/main.py
@@ -97,6 +97,41 @@ def prepare_data(data, window_size: int, forecast_horizon: int, train_ratio: str
     X, y, valid_indices = create_returns_sequence(data, window_size, forecast_horizon)
 
     n_samples = len(X)
+    train_size = int(n_samples * train_ratio)
+    val_size = int(n_samples * 0.15)
+
+    train_end = train_size
+    val_end = train_end + val_size
+
+    X_train = X[:train_end]
+    y_train = y[:train_end]
+
+    X_val = X[train_end:val_end]
+    y_val = y[train_end:val_end]
+
+    X_test = X[val_end:]
+    y_test = y[val_end:]
+
+    returns_scaler = StandardScaler()
+    X_train_scaled = returns_scaler.fit_transform(X_train)
+    X_val_scaled = returns_scaler.transform(X_val)
+    X_test_scaled = returns_scaler.transform(X_test)
+
+    target_scaler = StandardScaler()
+    y_train_scaled = target_scaler.fit_transform(y_train.reshape(-1, 1)).flatten()
+    y_val_scaled = target_scaler.transform(y_val.reshape(-1, 1)).flatten()
+    y_test_scaled = target_scaler.transform(y_test.reshape(-1, 1)).flatten()
+
+    return {
+        'X_train': X_train_scaled, 'y_train': y_train_scaled,
+        'X_val': X_val_scaled, 'y_val': y_val_scaled,
+        'X_test': X_test_scaled, 'y_test': y_test_scaled,
+        'returns_scaler': returns_scaler,
+        'target_scaler': target_scaler,
+        'y_train_original': y_train,
+        'y_val_original': y_val,
+        'y_test_original': y_test
+    }
 
 
 
