08d6bbb0926aa068bf7e525f682c36762a0b309e
lukaliegis <luka.liegis@gmail.com>
2025-06-19 12:57:35 +0300

    clean up

--- yield_curve_rv/main.py
+++ yield_curve_rv/main.py
@@ -1,8 +1,8 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
-import seaborn as sns
 
 
 def load_and_prepare_data():
