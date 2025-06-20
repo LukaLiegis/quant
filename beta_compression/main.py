a2436b9fe7dbe6668f0a489bfb98ca1fabe37aac
lukaliegis <luka.liegis@gmail.com>
2025-06-20 19:38:23 +0300

    get market data

--- beta_compression/main.py
+++ beta_compression/main.py
@@ -2,8 +2,18 @@ import pandas as pd
 import yfinance as yf
 
 
-def get_data():
-    ...
+def get_data(
+        tickers,
+        market_ticker: str = '^GSPC'
+):
+    all_tickers = tickers + market_ticker
+    data = yf.download(all_tickers, start='2000-01-01', end='2024-12-31', progress=False)['Close']
+    returns = data.pct_change().dropna()
+
+    market_returns = returns[market_ticker]
+    stock_returns = returns[tickers].dropna()
+
+    return stock_returns, market_returns
 
 
 def rolling_beta():
