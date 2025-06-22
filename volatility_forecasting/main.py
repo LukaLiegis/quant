3ca9eb0676cc0c926dbca554b2f86d2a419a460c
lukaliegis <luka.liegis@gmail.com>
2025-06-22 23:22:14 +0300

    fixed forecast window

--- volatility_forecasting/main.py
+++ volatility_forecasting/main.py
@@ -16,13 +16,13 @@ def get_data():
     return data
 
 
-def realized_vol(prices, window: int = 1):
+def realized_vol(
+        prices,
+        window: int = 63
+) -> pd.Series:
     returns = np.log(prices / prices.shift(1)).dropna()
 
-    if window == 1:
-        rv = np.abs(returns) * np.sqrt(252)
-    else:
-        rv = returns.rolling(window).std()* np.sqrt(252)
+    rv = returns.rolling(window).std() * np.sqrt(252)
 
     return rv.dropna()
 
